485cc9dd7198b00df5c022d71cc00f08f0a83b08
Henriette Kopstad <henriette.kopstad@gmail.com>
2014-10-15 18:42:53 -0700

    Added validation not really allready did it

--- test/models/community_test.rb
+++ test/models/community_test.rb
@@ -4,4 +4,10 @@ class CommunityTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+# test "Validate community name" do
+# c = Community.new = "hello"    or "bad"
+#assert.valid? //should be ok, bad should not be
+#end
+
 end
